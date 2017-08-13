93c5802dafc98446aad4da1caeb56268e60fc9b2
Kyle Chadwick <kylepchad@gmail.com>
2017-08-12 17:39:05 -0700

    Kinect gestures and standalone apps

---
+++ VisualStudio/StandaloneKinectViewer/StandaloneKinectViewer/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StandaloneKinectViewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //Initialization
+        KinectSensor _sensor;
+        MultiSourceFrameReader _reader;
+
+        _sensor = KinectSensor.GetDefault();
+
+        if (_sensor != null)
+        {
+            _sensor.Open();
+        }
+
+        //Reading the streams
+        _reader = _sensor.OpenMultiSourceFrameReader(FrameSourceTypes.Color |
+                                             FrameSourceTypes.Depth |
+                                             FrameSourceTypes.Infrared);
+        _reader = MultiSourceFrameArrive += Reader_MultiSourceFrameArrived;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
